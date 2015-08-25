129d172e284514aec62a08543cd0b5f55f4061f9
Pedro Caseiro <pcaseiro96@gmail.com>
2015-08-25 13:17:54 +0100

    fixed a bug in the popup divs

--- js/app.js
+++ js/app.js
@@ -1,114 +1,128 @@
-    var main = function(){
+var main = function(){
+    
+    //HIGHLIGHT E RESIZE DAS COORDENADAS
+    $('#myimage').mapster({
+        singleSelect: true,
+        fill : true,
+        fillOpacity : 0.6,
+        fillColor: '008FC5',
+        stroke: true,
+        strokeColor: '000000',
+        strokeOpacity: 1,
+        strokeWidth: 2,
+        onMouseover: function(e) {
+            $(this).mapster('set',false).mapster('set',true);
+        },
+        onMouseout: function(e) { 
+            $(this).mapster('set',false);
+        }
+    });
 
+    
+    //POP UP DAS DIVS COM TEXTO
 
-        // BOTÃO DO MENU! 
-        $('.icon-menu').click(function(){
-            $('.menu').animate(
-                {left: '0px'},
-                200);
-                
-            $('body').animate(
-            {left: '285px'}, 
-            200);
-        });
-        
-        $('.icon-close').click(function(){
-            $('.menu').animate({left: '-285px'},
-            200);
-            $('body').animate({left: '0px'},
-            200);
-        });
-        
-        //HIGHLIGHT E RESIZE DAS COORDENADAS
-        $('#myimage').mapster({
-            singleSelect: true,
-            fill : true,
-            fillOpacity : 0.6,
-            fillColor: '008FC5',
-            stroke: true,
-            strokeColor: '000000',
-            strokeOpacity: 1,
-            strokeWidth: 2,
-            onMouseover: function(e) {
-                $(this).mapster('set',false).mapster('set',true);
-            },
-            onMouseout: function(e) { 
-                $(this).mapster('set',false);
-            }
-        });
-        
-        //POP UP DAS DIVS COM TEXTO
-        // hides the slickbox as soon as the DOM is ready
-        $('#div1').hide();
+    /*
+    //Hide the div
+    $('#div1').hide();
 
-        // shows the slickbox on clicking the noted link  
-        $('#slick-show').click(function() {
-            $('#div1').show('slow');
-            return false;
-        });
+    // shows the slickbox on clicking the noted link  
+    $('#slick-show').click(function() {
+        $('#div1').show('slow');
+        return false;
+    });
 
-        // hides the slickbox on clicking the noted link  
-        $('#slick-hide').click(function() {
-            $('#div1').hide('fast');
-            return false;
-        });
+    // hides the slickbox on clicking the noted link  
+    $('#slick-hide').click(function() {
+        $('#div1').hide('fast');
+        return false;
+    });
 
-         
-        // toggles the slickbox on clicking the noted link  
-        $('#slick-toggle').click(function() {
-            $('#div1').toggle(400);
-            return false;
-        });           
-        
-        
+     
+    // toggles the slickbox on clicking the noted link  
+    $('#slick-toggle').click(function() {
+        $('#div1').toggle(400);
+        return false;
+    });           
+    */
+    
 
-        /*VIDEO JEKNOWLEDGE ACADEMY*/
-        $('.jekacademy').hide();
-        $('#popup2').click(function() {
-            $('.jekacademy').toggle(400);
-            return false;
-        });
+    /*VIDEO JEKNOWLEDGE ACADEMY*/
+    /*
+    $('.jekacademy').hide();
+    $('#popup2').click(function() {
+        $('.jekacademy').toggle(400);
+        return false;
+    });
 
-        
+    
 
-        $('.sjm').hide();
-        $('#sjmpopup').click(function(){
-            $('.sjm').toggle(400);
-            return false;
-        });
+    $('.sjm').hide();
 
-        $('#shfta').click(function(){
-            $('#div3').toggle(400);
-            return false;
-        });
+    $('#sjmpopup').click(function(){
+        $('.sjm').toggle(400);
+        return false;
+    });
+
+    $('#shfta').click(function(){
+        $('#div3').toggle(400);
+        return false;
+    });*/
 
 
 
 
 
-        // ADICIONAR TOOLTIPS!! :D
-    }  
+    // ADICIONAR TOOLTIPS!! :D
+}  
 
-    $(document).ready(main);
+$(document).ready(main);
 
-     ;(function($) {
+    ;(function($) {
             $(function() {
-                $('#trybpopup').on('click', function(e) {
+                $('#popupdiv1').on('click', function(e) {
+                    e.preventDefault();
+                    $('#div1').bPopup();
+                });
+             });
+         })(jQuery);
+
+
+    
+    ;(function($) {
+            $(function() {
+                $('#popupdiv2').on('click', function(e) {
                     e.preventDefault();
                     $('#div2').bPopup();
                 });
              });
          })(jQuery);
+
+
 
 
     ;(function($) {
             $(function() {
-                $('#tecno').on('click', function(e) {
+                $('#popupdiv4').on('click', function(e) {
                     e.preventDefault();
-                    $('#div2').bPopup();
+                    $('#div4').bPopup();
                 });
              });
          })(jQuery);
+
+    ;(function($) {
+            $(function() {
+                $('#popupdiv3').on('click', function(e) {
+                    e.preventDefault();
+                    $('#div3').bPopup();
+                });
+             });
+         })(jQuery);
+
+
+
+
+
+
 
 
 
