86d42a60e0886b9b2a5643c1102242e75f476b1d
pedrocaseiro <pcaseiro96@gmail.com>
2015-09-25 23:37:56 +0100

    Added second room + image mapping

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 var app = {
-    currentRoomImage: 'images/image.jpg',
+    currentRoomImage: 'images/door.jpg',
+
 
     changeRoom: function (newRoom) {
         this.currentRoomImage = newRoom;
 
+        
+
         $('#myimage').attr('src', this.currentRoomImage);
+
+
 
         // HIGHLIGHT E RESIZE DAS COORDENADAS
         $('#myimage').mapster({
@@ -30,7 +35,11 @@ var app = {
 };
 
 var main = function () {
-    app.changeRoom('images/image.jpg');
+
+
+
+    app.changeRoom('images/door.jpg');
+
 
     $('#mapster_wrap_0').css({
         'margin-left': 'auto',
@@ -56,116 +65,67 @@ var main = function () {
     $(window).bind('resize', function () {
         $('img').mapster('resize', 0, $(window).height(), 0);
     });
+
+
+    /* FUNÇÕES DE POPUP */
+    (function ($) {
+        $(function () {
+            $('#popupdiv1').on('click', function (e) {
+                e.preventDefault();
+                $('#div1').bPopup();
+            });
+        });
+    })(jQuery);
+
+
+    (function ($) {
+        $(function () {
+            $('#popupdiv2').on('click', function (e) {
+                e.preventDefault();
+                $('#div2').bPopup();
+            });
+        });
+    })(jQuery);
+
+
+    (function ($) {
+        $(function () {
+            $('#popupdiv3').on('click', function (e) {
+                e.preventDefault();
+                $('#div3').bPopup();
+            });
+        });
+    })(jQuery);
+
+
+    (function ($) {
+        $(function () {
+            $('#popupdiv4').on('click', function (e) {
+                e.preventDefault();
+                $('#div4').bPopup();
+            });
+        });
+    })(jQuery);
+
+
+    (function ($) {
+        $(function () {
+            $('#popupdiv5').on('click', function (e) {
+                e.preventDefault();
+                $('#div5').bPopup();
+            });
+        });
+
+    })(jQuery);
 };
 
 
 $(document).ready(main);
 
-//POP UP DAS DIVS COM TEXTO
-
-/*
- //Hide the div
- $('#div1').hide();
-
- // shows the slickbox on clicking the noted link
- $('#slick-show').click(function() {
- $('#div1').show('slow');
- return false;
- });
-
- // hides the slickbox on clicking the noted link
- $('#slick-hide').click(function() {
- $('#div1').hide('fast');
- return false;
- });
-
-
- // toggles the slickbox on clicking the noted link
- $('#slick-toggle').click(function() {
- $('#div1').toggle(400);
- return false;
- });
- */
-
-
-/*VIDEO JEKNOWLEDGE ACADEMY*/
-/*
- $('.jekacademy').hide();
- $('#popup2').click(function() {
- $('.jekacademy').toggle(400);
- return false;
- });
-
-
-
- $('.sjm').hide();
-
- $('#sjmpopup').click(function(){
- $('.sjm').toggle(400);
- return false;
- });
-
- $('#shfta').click(function(){
- $('#div3').toggle(400);
- return false;
- });*/
 
 
 
 
-
-// ADICIONAR TOOLTIPS!! :D
-
-
-(function ($) {
-    $(function () {
-        $('#popupdiv1').on('click', function (e) {
-            e.preventDefault();
-            $('#div1').bPopup();
-        });
-    });
-})(jQuery);
-
-
-(function ($) {
-    $(function () {
-        $('#popupdiv2').on('click', function (e) {
-            e.preventDefault();
-            $('#div2').bPopup();
-        });
-    });
-})(jQuery);
-
-
-(function ($) {
-    $(function () {
-        $('#popupdiv3').on('click', function (e) {
-            e.preventDefault();
-            $('#div3').bPopup();
-        });
-    });
-})(jQuery);
-
-
-(function ($) {
-    $(function () {
-        $('#popupdiv4').on('click', function (e) {
-            e.preventDefault();
-            $('#div4').bPopup();
-        });
-    });
-})(jQuery);
-
-
-(function ($) {
-    $(function () {
-        $('#popupdiv5').on('click', function (e) {
-            e.preventDefault();
-            $('#div5').bPopup();
-        });
-    });
-
-})(jQuery);
 
 
 
