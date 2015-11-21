f0e391ed79533054d86622ef2bbd202aeab6777f
inesilva <ines_ngc@hotmail.com>
2015-11-21 17:08:55 +0000

    added function for innovation-walloffame (1)

--- js/app.js
+++ js/app.js
@@ -331,6 +331,15 @@ var main = function () {
         });
     })(jQuery);
     
+    
+    (function ($) {
+        $(function () {
+            $('#innovation-walloffame').on('click', function (e) {
+                e.preventDefault();
+                $('#div-innovation-walloffame').bPopup();
+            });
+        });
+    })(jQuery);
 
 
     (function ($) {
@@ -342,14 +351,6 @@ var main = function () {
         });
     })(jQuery);
     
-    (function ($) {
-        $(function () {
-            $('#innovation-walloffame').on('click', function (e) {
-                e.preventDefault();
-                $('#div-innovation-walloffame').bPopup();
-            });
-        });
-    })(jQuery);
 
     (function ($) {
         $(function () {
